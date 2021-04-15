2680ed8adc85fa921d9adcdc0c5d3f621144608c
SurgNick <nicolas.cavani@surgar-surgery.com>
2021-04-15 17:59:17 +0200

    try stuff

--- source/src/mylib/mylib.hpp
+++ source/src/mylib/mylib.hpp
@@ -2,6 +2,16 @@
 #ifndef MYLIB_HPP
 #define MYLIB_HPP
 
-int insaneMath(int);
+#if defined(_WIN32)
+#  if defined(TUTO_EXPORTING_MYLIB)
+#    define DECLSPEC __declspec(dllexport)
+#  else
+#    define DECLSPEC __declspec(dllimport)
+#  endif
+#else // non windows
+#  define DECLSPEC
+#endif
+
+__declspec(noinline) int insaneMath(int);
 
 #endif
