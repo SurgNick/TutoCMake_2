872db2aa0f10e25d56df46333931d492a8e8ff72
SurgNick <nicolas.cavani@surgar-surgery.com>
2021-04-16 17:04:06 +0200

    add _export.h file, but no more .lib file is created

--- source/src/mylib/mylib.hpp
+++ source/src/mylib/mylib.hpp
@@ -1,14 +1,43 @@
 
 #pragma once
 
-#if defined(_WIN32)
-#  if defined(TUTO_EXPORTING_MYLIB)
-#    define DECLSPEC __declspec(dllexport)
-#  else
-#    define DECLSPEC __declspec(dllimport)
+// #include "TutoCMakeLib_export.h"
+
+#ifdef TUTOCMAKELIB_STATIC_DEFINE
+#  define TUTO_MYLIB_EXPORT
+#  define TUTOCMAKELIB_NO_EXPORT
+#else
+#  ifndef TUTO_MYLIB_EXPORT
+#    ifdef TutoCMakeLib_EXPORTS
+        /* We are building this library */
+#      define TUTO_MYLIB_EXPORT __declspec(dllexport)
+#    else
+        /* We are using this library */
+#      define TUTO_MYLIB_EXPORT __declspec(dllimport)
+#    endif
 #  endif
-#else // non windows
-#  define DECLSPEC
+
+#  ifndef TUTOCMAKELIB_NO_EXPORT
+#    define TUTOCMAKELIB_NO_EXPORT 
+#  endif
 #endif
 
-DECLSPEC int insaneMath(int);
+#ifndef TUTOCMAKELIB_DEPRECATED
+#  define TUTOCMAKELIB_DEPRECATED __declspec(deprecated)
+#endif
+
+#ifndef TUTOCMAKELIB_DEPRECATED_EXPORT
+#  define TUTOCMAKELIB_DEPRECATED_EXPORT TUTO_MYLIB_EXPORT TUTOCMAKELIB_DEPRECATED
+#endif
+
+#ifndef TUTOCMAKELIB_DEPRECATED_NO_EXPORT
+#  define TUTOCMAKELIB_DEPRECATED_NO_EXPORT TUTOCMAKELIB_NO_EXPORT TUTOCMAKELIB_DEPRECATED
+#endif
+
+#if 0 /* DEFINE_NO_DEPRECATED */
+#  ifndef TUTOCMAKELIB_NO_DEPRECATED
+#    define TUTOCMAKELIB_NO_DEPRECATED
+#  endif
+#endif
+
+/*TUTO_MYLIB_EXPORT */int insaneMath(int);
