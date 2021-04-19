ca9e3f8285dda601b8391a8dd428cc9565865702
SurgNick <nicolas.cavani@surgar-surgery.com>
2021-04-19 10:43:33 +0200

    fix .lib not found when using the SHARED library but done with : WINDOWS_EXPORT_ALL_SYMBOLS ON ; will need to find something else

--- source/src/mylib/mylib.hpp
+++ source/src/mylib/mylib.hpp
@@ -1,43 +1,6 @@
 
 #pragma once
 
-// #include "TutoCMakeLib_export.h"
-
-#ifdef TUTOCMAKELIB_STATIC_DEFINE
-#  define TUTO_MYLIB_EXPORT
-#  define TUTOCMAKELIB_NO_EXPORT
-#else
-#  ifndef TUTO_MYLIB_EXPORT
-#    ifdef TutoCMakeLib_EXPORTS
-        /* We are building this library */
-#      define TUTO_MYLIB_EXPORT __declspec(dllexport)
-#    else
-        /* We are using this library */
-#      define TUTO_MYLIB_EXPORT __declspec(dllimport)
-#    endif
-#  endif
-
-#  ifndef TUTOCMAKELIB_NO_EXPORT
-#    define TUTOCMAKELIB_NO_EXPORT 
-#  endif
-#endif
-
-#ifndef TUTOCMAKELIB_DEPRECATED
-#  define TUTOCMAKELIB_DEPRECATED __declspec(deprecated)
-#endif
-
-#ifndef TUTOCMAKELIB_DEPRECATED_EXPORT
-#  define TUTOCMAKELIB_DEPRECATED_EXPORT TUTO_MYLIB_EXPORT TUTOCMAKELIB_DEPRECATED
-#endif
-
-#ifndef TUTOCMAKELIB_DEPRECATED_NO_EXPORT
-#  define TUTOCMAKELIB_DEPRECATED_NO_EXPORT TUTOCMAKELIB_NO_EXPORT TUTOCMAKELIB_DEPRECATED
-#endif
-
-#if 0 /* DEFINE_NO_DEPRECATED */
-#  ifndef TUTOCMAKELIB_NO_DEPRECATED
-#    define TUTOCMAKELIB_NO_DEPRECATED
-#  endif
-#endif
+#include "TutoCMakeLib_export.h"
 
 /*TUTO_MYLIB_EXPORT */int insaneMath(int);
